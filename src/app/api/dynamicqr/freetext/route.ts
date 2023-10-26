14c1b522a6bff099660a14b2677f503aa5855308
Mohd Ziyad <mziyadc@gmail.com>
2023-10-26 22:14:29 +0530

    POST request created for freetext dynamic

---
+++ src/app/api/dynamicqr/freetext/route.ts
@@ -0,0 +1,43 @@
+import { getAuthSession } from "@/lib/auth";
+import { prismaClient } from "@/lib/db";
+import { getBase64UUID } from "@/lib/utils";
+import { dynamicFreeTextFormSchema } from "@/validators/qrFormSchema";
+import { NextResponse } from "next/server";
+import QRCode from "qrcode";
+
+export async function POST(req: Request, res: Response) {
+  try {
+    const session = await getAuthSession();
+    if (!session?.user) {
+      return new NextResponse("User Unauthorized", { status: 401 });
+    }
+    const body = await req.json();
+    const { name, text } = dynamicFreeTextFormSchema.parse(body);
+
+    const base64Uuid = getBase64UUID();
+    const encodedUuid = encodeURIComponent(base64Uuid);
+    const generateQR = await QRCode.toDataURL(
+      `http://localhost:3000/dynamicfreetext/${encodedUuid}`,
+      {
+        errorCorrectionLevel: "M",
+        rendererOpts: {
+          quality: 1,
+        },
+      }
+    );
+    await prismaClient.dynamicFreeText.create({
+      data: {
+        name: name,
+        freetext: text,
+        userId: session.user.id,
+        qrCode: generateQR,
+        uniqueToken: base64Uuid,
+      },
+    });
+    return NextResponse.json({ qrCode: generateQR }, { status: 200 });
+  } catch (error) {
+    return new NextResponse("[INTERNAL SERVOR ERROR] " + error, {
+      status: 500,
+    });
+  }
+}
