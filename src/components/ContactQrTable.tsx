3c14b7952dc67d5b6a460b66983f8f4a65f15add
Mohd Ziyad <mziyadc@gmail.com>
2023-10-28 16:23:40 +0530

    SSR added for fetching contact qr's

--- src/components/ContactQrTable.tsx
+++ src/components/ContactQrTable.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import {
   Table,
   TableHead,
@@ -22,6 +22,9 @@ import {
 import { DynamicContact } from "@prisma/client";
 import { useToast } from "./ui/use-toast";
 import { Edit2, Trash2 } from "lucide-react";
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+import LoadingSpinner from "@/app/manage/loading";
 
 type Props = {};
 
@@ -31,50 +34,130 @@ const ContactQrTable = (props: Props) => {
   const [qrCodes, setQRCodes] = useState<DynamicContact[]>([]);
   const [qrCode, setQrCode] = useState<DynamicContact>();
   const { toast } = useToast();
+
+  const {
+    data: contactQrCodes,
+    isLoading,
+    isSuccess,
+  } = useQuery({
+    queryKey: ["dynamicContactQr"],
+    queryFn: async () => {
+      const response = await axios.get("/api/dynamicqr/contact");
+      return response.data.qrCodes;
+    },
+  });
+
+  useEffect(() => {
+    if (isSuccess) {
+      setQRCodes(contactQrCodes);
+    }
+  }, [contactQrCodes, isSuccess]);
+
+  if (isLoading) {
+    return (
+      <>
+        <LoadingSpinner component />
+      </>
+    );
+  }
+  console.log("QR CODES: ", qrCodes?.length);
+
   return (
     <div>
-      <Table>
-        <TableHeader>
-          <TableRow>
-            <TableHead>S.No</TableHead>
-            <TableHead>Contact</TableHead>
-            <TableHead>Edit</TableHead>
-          </TableRow>
-        </TableHeader>
-        <TableBody>
-          {qrCodes?.map((qrCode, idx) => (
-            <TableRow key={idx}>
-              <TableCell>{idx + 1}</TableCell>
-              <TableCell>
-                {qrCode.firstName} {qrCode.lastName}
-              </TableCell>
+      {qrCodes.length !== 0 ? (
+        <>
+          <Table>
+            <TableHeader>
+              <TableRow>
+                <TableHead>S.No</TableHead>
+                <TableHead>Contact</TableHead>
+                <TableHead>Edit</TableHead>
+              </TableRow>
+            </TableHeader>
+            <TableBody>
+              {qrCodes.map((qrCode, idx) => (
+                <TableRow key={idx}>
+                  <TableCell>{idx + 1}</TableCell>
+                  <TableCell>
+                    {qrCode.firstName} {qrCode.lastName}
+                  </TableCell>
 
-              <TableCell>
-                <div className="flex flex-row justify-start items-center">
-                  <div
-                    className="hover:bg-secondary-foreground/10 rounded-md w-fit p-2"
-                    onClick={() => {
-                      setEditDialog(true);
-                      setQrCode(qrCode);
-                    }}
-                  >
-                    <Edit2 className="h-4 w-4 " />
-                  </div>
-                  <div
-                    className="ml-1 text-red-500 hover:bg-secondary-foreground/10 w-fit p-2 rounded-md"
-                    onClick={() => {
-                      setQrCode(qrCode);
-                      setDeleteDialog(true);
-                    }}
-                  >
-                    <Trash2 className="h-4 w-4" />
-                  </div>
-                </div>
-              </TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
+                  <TableCell>
+                    <div className="flex flex-row justify-start items-center">
+                      <div
+                        className="hover:bg-secondary-foreground/10 rounded-md w-fit p-2"
+                        onClick={() => {
+                          setEditDialog(true);
+                          setQrCode(qrCode);
+                        }}
+                      >
+                        <Edit2 className="h-4 w-4 " />
+                      </div>
+                      <div
+                        className="ml-1 text-red-500 hover:bg-secondary-foreground/10 w-fit p-2 rounded-md"
+                        onClick={() => {
+                          setQrCode(qrCode);
+                          setDeleteDialog(true);
+                        }}
+                      >
+                        <Trash2 className="h-4 w-4" />
+                      </div>
+                    </div>
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+          <Dialog open={editDialog} onOpenChange={setEditDialog}>
+            <DialogContent>
+              <DialogHeader>
+                <DialogTitle>Edit QR Code</DialogTitle>
+              </DialogHeader>
+              {/* {qrCode && (
+            <UpdateFreeTextForm
+              qrCode={qrCode}
+              editDialog={editDialog}
+              setEditDialog={setEditDialog}
+            />
+          )} */}
+            </DialogContent>
+          </Dialog>
+          <AlertDialog open={deleteDialog} onOpenChange={setDeleteDialog}>
+            <AlertDialogContent>
+              <AlertDialogHeader>
+                <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+                <AlertDialogDescription>
+                  This will permanently delete this QR code. This cannot be
+                  undone.
+                </AlertDialogDescription>
+              </AlertDialogHeader>
+              <AlertDialogFooter>
+                <AlertDialogCancel>Cancel</AlertDialogCancel>
+                <AlertDialogAction
+                  className="bg-red-500"
+                  onClick={() => {
+                    // deleteFreeTextQR(qrCode!.uniqueToken);
+                    // if (isDeleted) {
+                    //   setDeleteDialog(false);
+                    // }
+                  }}
+                >
+                  <p>Delete</p>
+                  {/* {isDeleting ? (
+                <Loader2 className="h-4 w-4 animate-spin" />
+              ) : (
+                <p>Delete</p>
+              )} */}
+                </AlertDialogAction>
+              </AlertDialogFooter>
+            </AlertDialogContent>
+          </AlertDialog>
+        </>
+      ) : (
+        <div className="flex justify-center items-center">
+          <p>No Contact QR Codes found</p>
+        </div>
+      )}
     </div>
   );
 };
