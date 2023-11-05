7e55039894d8e387ddd8e5188f4175928cc1e98c
Mohd Ziyad <mziyadc@gmail.com>
2023-11-05 19:18:00 +0530

    Ai QR and Dynamic Qr tabs added to /manage

--- src/app/manage/page.tsx
+++ src/app/manage/page.tsx
@@ -8,6 +8,9 @@ import DynamicQRCard from "@/components/DynamicQRCard";
 import MultiURLTable from "@/components/MultiURLTable";
 import FreeTextTable from "@/components/FreeTextTable";
 import ContactQrTable from "@/components/ContactQrTable";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { ArrowLeft } from "lucide-react";
+import Link from "next/link";
 
 type Props = {};
 
@@ -26,20 +29,33 @@ const Manage = async (props: Props) => {
   // });
   // control tabs programmatically.
   return (
-    <div className="flex flex-col p-4">
+    <div className="flex flex-col p-4 gap-4">
+      <h2>
+        <Link href={"/dashboard"}>
+          <ArrowLeft className="h-6 w-6" />
+        </Link>
+      </h2>
       <h1 className="font-bold text-3xl">Your QR Codes</h1>
       {/* <h3 className="font-semibold text-xl mt-8 mb-4">Dynamic URL QR Codes</h3> */}
-
-      <DynamicQRCard
-        dynamicURLTable={
-          <Suspense fallback={<LoadingSpinner component={true} />}>
-            <DynamicURLTable />
-          </Suspense>
-        }
-        multiURLTable={<MultiURLTable />}
-        freeTextTable={<FreeTextTable />}
-        contactQrTable={<ContactQrTable />}
-      />
+      <Tabs defaultValue="aiqrcode">
+        <TabsList className="grid w-2/12 grid-cols-2">
+          <TabsTrigger value="aiqrcode">AI QR</TabsTrigger>
+          <TabsTrigger value="dynamic">Dynamic QR</TabsTrigger>
+        </TabsList>
+        <TabsContent value="aiqrcode">Ai QR Code Tab</TabsContent>
+        <TabsContent value="dynamic">
+          <DynamicQRCard
+            dynamicURLTable={
+              <Suspense fallback={<LoadingSpinner component={true} />}>
+                <DynamicURLTable />
+              </Suspense>
+            }
+            multiURLTable={<MultiURLTable />}
+            freeTextTable={<FreeTextTable />}
+            contactQrTable={<ContactQrTable />}
+          />
+        </TabsContent>
+      </Tabs>
     </div>
   );
 };
