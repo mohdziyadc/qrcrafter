4ea307933cf0cc6d843eaaa559317b0b3409e3c3
Mohd Ziyad <mziyadc@gmail.com>
2023-11-09 18:29:08 +0530

    POST request created for deleting ai url qr codes

---
+++ src/app/api/aiqrcode/url/delete/route.ts
@@ -0,0 +1,23 @@
+import { getAuthSession } from "@/lib/auth";
+import { prismaClient } from "@/lib/db";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function POST(req: NextRequest) {
+  try {
+    const session = await getAuthSession();
+    if (!session?.user) {
+      return new NextResponse("User Unauthorized", { status: 401 });
+    }
+    const { uniqueToken } = await req.json();
+    await prismaClient.aiURLQRCode.delete({
+      where: {
+        uniqueToken: uniqueToken,
+      },
+    });
+    return new NextResponse("[SUCCESS]", { status: 200 });
+  } catch (error) {
+    return new NextResponse("[INTERNAL SERVOR ERROR] " + error, {
+      status: 500,
+    });
+  }
+}
