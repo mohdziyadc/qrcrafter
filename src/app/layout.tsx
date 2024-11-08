efc89ec753d90be32e606f78edfd397c1e9a4dc3
Mohd Ziyad <mziyadc@gmail.com>
2024-11-08 23:00:56 +0530

    Added favicon and scripts for railway

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,6 +23,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className="dark">
+      <head>
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+      </head>
       <body className={inter.className}>
         <Providers>
           {children}
