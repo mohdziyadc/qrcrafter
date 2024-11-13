6725000eeea4a7121172ed944b6080fd0735b799
Mohd Ziyad <mziyadc@gmail.com>
2024-11-13 21:30:28 +0530

    Added privacy policy in footer + loading comp added

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,12 +1,35 @@
 import { Copyright } from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
 type Props = {};
 
+const LOGO_URL =
+  "https://pub-c39a57a5d64440d1a0abfeecdb85f452.r2.dev/qrCrafter-Logo.png";
+
 const Footer = (props: Props) => {
   return (
-    <section className="bg-primary-foreground py-4 flex justify-center items-center">
-      <div className="flex justify-center text-primary/70 items-center">
+    <section className="bg-primary-foreground pt-4 pb-2 fle flex-col justify-center items-center">
+      <div className="flex flex-row mb-4 px-8 justify-between items-center w-full">
+        <div className="flex gap-4 justify-center items-center">
+          <Image
+            src={LOGO_URL}
+            width={40}
+            height={40}
+            alt="logo"
+            className="rounded-sm"
+          />
+          <div className="text-3xl font-bold">QRCoded</div>
+        </div>
+        <div className="flex gap-4 ">
+          <div>
+            <Link href={"/privacy-policy"}>Privacy Policy</Link>
+          </div>
+          <div>Terms &amp; Conditions</div>
+        </div>
+      </div>
+      <div className="flex justify-center text-sm text-primary/70 items-center">
         Copyright <Copyright className="h-4 w-4 mx-1" /> 2024 QRCoded. All
         rights reserved
       </div>
